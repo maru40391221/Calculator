5b57c646b9ddf234cca14a5dcc88dd792643a91c
maru40391221 <polorll.1221@gmail.com>
2023-06-23 18:15:22 +0900

    button animation and enter/clear function

--- script.js
+++ script.js
@@ -43,3 +43,33 @@ console.log(operate("-", firstNum, secondNum));  // Output: -10
 console.log(operate("*", firstNum, secondNum));  // Output: 200
 console.log(operate("/", firstNum, secondNum));  // Output: 0.5
 
+
+// *************************enter/clear*************************
+let pressTimer;
+
+enterAndClear.addEventListener('mousedown', function() {
+  // Clear any existing timeout to start fresh
+  clearTimeout(pressTimer);
+  
+  // Set timeout to run function after 3 seconds
+  pressTimer = setTimeout(function() {
+    // Change the background color
+    enterAndClear.style.backgroundColor = "rgba(75, 63, 243, 0.845)";
+    screen.textContent = "";
+    display= [];
+  }, 500);
+
+})
+
+
+enterAndClear.addEventListener('mouseup', function() {
+  // Clear the timeout if the button is released before 3 seconds
+  clearTimeout(pressTimer);
+
+  pressTimer = setTimeout(function() {
+    // Change the background color
+    enterAndClear.style.backgroundColor = "rgba(255, 0, 0, 0.845)"; // Change this color as you wish
+  }, 250);
+})
+
+// **************************************************
